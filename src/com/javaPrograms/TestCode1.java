2365d66f58a76c56e211b5c10786eb5525fc9659
ragha <ragha@Raghava>
2024-10-09 16:36:50 +0530

    My First commit

---
+++ src/com/javaPrograms/TestCode1.java
@@ -0,0 +1,11 @@
+package com.javaPrograms;
+
+public class TestCode1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("heloo world");
+
+	}
+
+}
